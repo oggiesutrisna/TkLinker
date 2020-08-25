9f744f3836c4a2b65f6687eea81cd8986872f523
oggiesutrisna <40566790+oggiesutrisna@users.noreply.github.com>
2020-08-25 22:01:54 +0800

    pylint integrating

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-import pyshorteners
+import pyshorteners #import this as the library for shorteners
 
 root = tk.Tk()
 
 canvas1 = tk.Canvas(root, width=400, height=300, relief='raised')
-canvas1.pack()
+canvas1.pack() #canvas to take out the agile 
 
 label1 = tk.Label(root, text='Shortner Link Personal Use version')
 label1.config(font=('helvetica', 14))
@@ -24,7 +24,7 @@ def getLinker():
     label3 = tk.Label(root, text='The Shortlink is ' + x1 + ' is:', font=('helvetica', 10))
     canvas1.create_window(200, 210, window=label3)
 
-    label4 = tk.Label(root, text=pyshorteners.Shortener().tinyurl.short(x1))
+    label4 = tk.Label(root, label=pyshorteners.Shortener().tinyurl.short(x1))
     canvas1.create_window(200, 230, window=label4)
 
 
