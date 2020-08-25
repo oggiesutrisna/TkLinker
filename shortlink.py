e9211035edc16e16551d9995fa9bf3cf54f37eb5
oggiesutrisna <40566790+oggiesutrisna@users.noreply.github.com>
2020-08-25 12:33:10 +0800

    tklinker

---
+++ shortlink.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import pyshorteners
+
+root = tk.Tk()
+
+canvas1 = tk.Canvas(root, width=400, height=300, relief='raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Shortner Link for Personal-Use')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(200, 25, window=label1)
+
+label2 = tk.Label(root, text='Paste the link that you copied')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(200, 100, window=label2)
+
+entry1 = tk.Entry(root)
+canvas1.create_window(200, 140, window=entry1)
+
+
+def getSquareRoot():
+    x1 = entry1.get()
+
+    label3 = tk.Label(root, text='The Shortlink is ' + x1 + ' is:', font=('helvetica', 10))
+    canvas1.create_window(200, 210, window=label3)
+
+    label4 = tk.Label(root, text=pyshorteners.Shortener().tinyurl.short(x1))
+    canvas1.create_window(200, 230, window=label4)
+
+
+button1 = tk.Button(text='Get Shortner Link', command=getSquareRoot, bg='brown', fg='white',
+                    font=('helvetica', 9, 'bold'))
+canvas1.create_window(200, 180, window=button1)
+
+root.mainloop()
